140389c68bbcc7e5103c7d23e3dd3f71cd88f6a4
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2013-01-16 21:54:19 -0300

    gallery maked

--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
 $('document').ready(function(){
 	
 	//Stores
-	var $window = $(window);
+	var $gallery = $('#gallery'),
+		$f = $gallery.find('figure'),
+		
+		nro = 20;
+		
+	
+		
+		for(var a = 0;a<nro;a++){
+			var $nf = $f.clone();
+			var img = $nf.find('img');
+			
+			var src = img.attr('src')+'?'+Math.floor(Math.random()*9999999);
+			img.attr('src',src);
+			
+			
+			$gallery.append($nf.show());
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
 	
 });
